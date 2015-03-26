7edbabb521cc98c195e92a81ae0510dd99f36c4d
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-03-26 10:20:14 +0100

    Izmijenio footer (text).

--- first/index.php
+++ first/index.php
@@ -92,7 +92,8 @@
 		    <h1><img src="img/logo.png" alt="LOGO"></h1>
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
-		    <h1 style="color:#3f007f;">Mobile Site<a href="http://www.caseysurgicalarts.com/" rel="external">  Clasic Site</a></h1>
+
+            <h1 style="color:#3f007f;">Powered by <a href="http://mobilegrows.com/">MobileGrows</a>|<a href="http://www.caseysurgicalarts.com/" rel="external">  Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
